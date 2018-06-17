d4997038ff738732d94a30df8f8e82e0cebd5262
coco66 <heejinj@seas.upenn.edu>
2018-06-17 17:43:30 -0400

    deepadfq, test, steps printing

--- DeepADFQ/run_atari.py
+++ DeepADFQ/run_atari.py
@@ -116,12 +116,14 @@ def test():
     while True:
         obs, done = env.reset(), False
         episode_rew = 0
+        t = 0
         while not done:
             if not(args.record):
                 env.render()
             obs, rew, done, _ = env.step(act(obs[None])[0])
             episode_rew += rew
-        print("Episode reward", episode_rew)
+            t += 1 
+        print("Episode reward %.2f after %d steps"%(episode_rew, t))
 
 def plot(records, directory):
     import matplotlib
