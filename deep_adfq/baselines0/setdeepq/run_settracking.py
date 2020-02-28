d2d5fada0e382a875d5cea977e8b879673851cef
Christopher Hsu <christoherhsu8@gmail.com>
2020-02-28 13:16:55 -0500

    run files use eval class

--- deep_adfq/baselines0/setdeepq/run_settracking.py
+++ deep_adfq/baselines0/setdeepq/run_settracking.py
@@ -122,7 +122,6 @@ def train(seed, save_dir):
         env.moviewriter.finish()
 
 def test(seed):
-    set_global_seeds(seed)
     learning_prop = json.load(open(os.path.join(args.log_dir, 'learning_prop.json'),'r'))
     env = envs.make(args.env,
                     'ma_target_tracking',
@@ -135,83 +134,13 @@ def test(seed):
                     num_targets=args.nb_targets, #learning_prop['nb_targets'],
                     is_training=False,
                     )
-    timelimit_env = env
-    while( not hasattr(timelimit_env, '_elapsed_steps')):
-        timelimit_env = timelimit_env.env
+
     act_params = {'scope': "seed_%d"%learning_prop['seed']+"/"+learning_prop['scope'], 'eps': args.test_eps}
     act = setdeepq.load(os.path.join(args.log_dir, args.log_fname), act_params)
 
-    if args.ros_log:
-        from envs.target_tracking.ros_wrapper import RosLog
-        ros_log = RosLog(num_targets=args.nb_targets, wrapped_num=args.ros + args.render + args.record + 1)
-
-    ep = 0
-    init_pos = []
-    ep_nlogdetcov = ['Episode nLogDetCov']
-    time_elapsed = ['Elapsed Time (sec)']
-    graph_nlogdetcov = []
-    given_init_pose, test_init_pose = [], []
-    # Use a fixed set of initial positions if given.
-    if args.init_file_path != '.':
-        import pickle
-        given_init_pose = pickle.load(open(args.init_file_path, "rb"))
-
-    while(ep < args.nb_test_steps): # test episode
-        ep += 1
-        episode_rew, nlogdetcov = 0, 0
-        done = {}
-        obs = env.reset(init_pose_list=given_init_pose)
-        test_init_pose.append({'agents':[timelimit_env.env.agents[i].state for i in range(args.nb_agents)],
-                            'targets':[timelimit_env.env.targets[i].state for i in range(args.nb_targets)],
-                            'belief_targets':[timelimit_env.env.belief_targets[i].state for i in range(args.nb_targets)]})
-        s_time = time.time()
-
-        action_dict = {}
-        while type(done) is dict:
-            if args.render:
-                env.render()
-            if args.ros_log:
-                ros_log.log(env)
-            for agent_id, a_obs in obs.items():
-                action_dict[agent_id] = act(np.array(a_obs)[None])[0]
-            obs, rew, done, info = env.step(action_dict)
-            episode_rew += rew['__all__']
-            nlogdetcov += info['mean_nlogdetcov']
-
-        time_elapsed.append(time.time() - s_time)
-        ep_nlogdetcov.append(nlogdetcov)
-        graph_nlogdetcov.append(nlogdetcov)
-        if ep % 100 == 0:
-            print("Ep.%d - Episode reward : %.2f, Episode nLogDetCov : %.2f"%(ep, episode_rew, nlogdetcov))
-
-    if args.record :
-        env.moviewriter.finish()
-    if args.ros_log :
-        ros_log.save(args.log_dir)
-
-    # Eval plots and saves
-    eval_dir = os.path.join(args.log_dir, 'eval_seed%d'%(seed))
-    if not os.path.exists(eval_dir):
-        os.makedirs(eval_dir)
-    import pickle, tabulate
-    import matplotlib
-    matplotlib.use('Agg')
-    from matplotlib import pyplot as plt
-    f0, ax0 = plt.subplots()
-    _ = ax0.plot(graph_nlogdetcov, '.')
-    _ = ax0.set_xlabel('episode number')
-    _ = ax0.set_ylabel('mean_nlogdetcov')
-    _ = ax0.grid()
-    _ = f0.savefig(os.path.join(eval_dir, "%da%dt_%d_eval_eps.png"%
-                                        (args.nb_agents, args.nb_targets, args.nb_test_steps)))
-    plt.close()
-    pickle.dump(graph_nlogdetcov, open(os.path.join(eval_dir,'%da%dt_%d_eval_eps.pkl'%
-                                        (args.nb_agents, args.nb_targets, args.nb_test_steps)), 'wb'))
-    
-    pickle.dump(test_init_pose, open(os.path.join(args.log_dir,'test_init_pose.pkl'), 'wb'))
-    f_result = open(os.path.join(args.log_dir, 'test_result.txt'), 'w')
-    f_result.write(tabulate.tabulate([ep_nlogdetcov, time_elapsed], tablefmt='presto'))
-    f_result.close()
+    from baselines0.evaluation import Test
+    Eval = Test()
+    Eval.test(args, env, act)
 
 if __name__ == '__main__':
     if args.mode == 'train':
